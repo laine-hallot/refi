c3d325dbeca2ad33e9b3b515ed24d91f718bc012
Laine Hallot <lainehallot@gmail.com>
2026-05-21 13:35:28 -0500

    refactor: move polyfils into runtime package

--- packages/runtime/src/polyfills/pollyfils.d.ts
+++ packages/runtime/src/polyfills/pollyfils.d.ts
@@ -1,13 +1,24 @@
 declare interface Console {
-  log: (text: string) => void;
-  error: (text: string | Error) => void;
+  log: (...text: any[]) => void;
+  error: (...text: any[]) => void;
 }
 
 declare interface Performance {
   now: () => number;
 }
+
 declare function setTimeout(fn: () => any, ms: number): number;
 declare function clearTimeout(id: number): void;
 declare function setImmediate(fn: () => any): number;
 declare function clearImmediate(id: number): void;
 declare function queueMicrotask(fn: () => any): void;
+
+type HostPolyfills = {
+  hasWork: () => boolean;
+  drainMicrotasks: () => void;
+  tick: () => void;
+};
+
+declare var __host: HostPolyfills;
+
+declare var console: Console;
