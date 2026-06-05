14936a123769acadc349635bdabae1139566922f
Laine Hallot <lainehallot@gmail.com>
2026-06-05 15:16:44 -0500

    refactor(runtime): export promethee contstants

--- packages/runtime/index.ts
+++ packages/runtime/index.ts
@@ -2,7 +2,7 @@ import './src/polyfills/index.js';
 
 import refiPointer from './src/refi-pointer';
 export type { PointerInfo } from './src/refi-pointer';
-
-export * from './external/promethee/types';
+export type * from './external/promethee/types';
+export * from './external/promethee/constants';
 export { getScreenSize } from './src/graphics';
 export { refiPointer };
